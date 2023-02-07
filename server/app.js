8015de8411e572a708287a3e54bae5fe58a328db
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-02-08 02:34:28 +0500

    changes

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+const morgan = require("morgan");
 
 const planetsRouter = require("./routes/planets.router");
 
@@ -11,6 +13,14 @@ app.use(
   })
 );
 
+//morgan is a logger middleware for node.js
+app.use(morgan("short"));
+
 app.use(planetsRouter);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 module.exports = app;
