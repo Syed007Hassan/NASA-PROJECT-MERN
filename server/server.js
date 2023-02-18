5c180b17a295006b83d4c1e4353037e0409861b2
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-02-18 11:40:21 +0500

    mongo server added

--- server/server.js
+++ server/server.js
@@ -1,16 +1,32 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose.set("strictQuery", false);
 
 const { loadPlanetsData } = require("./models/planets.model");
 
 const port = 5000 || process.env.PORT;
 
 async function startServer() {
+  await connectDb();
   await loadPlanetsData();
 
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
 }
+//creating a db connection
+const connectDb = async () => {
+  await mongoose.connect(
+    "mongodb+srv://admin-hassan:" +
+      process.env.MONGOATLASPASWORD +
+      "@cluster0.yipmq.mongodb.net/NasaProject"
+  );
+  console.log("MongoDB Server is up and running");
+};
 
 startServer();
 module.exports = app;
